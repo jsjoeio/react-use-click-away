7f22f68200711f55186bef1e8845bab0dbb28fe7
Joe Previte <jjprevite@gmail.com>
2019-05-31 13:24:38 -0700

    Add better error handling

--- src/index.js
+++ src/index.js
@@ -8,8 +8,31 @@ import { useEffect } from 'react'
  * @property {array} clickable - an array of element ids which can be clicked which won't close the menus/containers
  **/
 export default function useClickAway ({ reactAppId, clickable, open, setOpen }) {
-  if (clickable.length < 1) throw Error('`clickable` property must contain at least one element id. Try passing an array of element ids to useClickAway({ clickable: [\'element1\', \'element2\'] })')
-  if (clickable.some(e => e.includes('#'))) console.warn('Element ids in `clickable` array should not be prefixed with a `#` symbol.')
+  // Check if they passed options object
+  if (arguments.length < 1) {
+    console.error('Oops! Looks like you forgot to pass the options object to `useClickAway`.')
+    return
+  }
+
+  // If they passed the objects object
+  if(arguments[0]) {
+    const expectedProps = ['open', 'setOpen', 'reactAppId', 'clickable']
+    // Check for all props
+    for (let prop of expectedProps) {
+      if (!arguments[0].hasOwnProperty(prop)) {
+        console.error(`The options object is missing the ${prop} property.`)
+      }
+    }
+  }
+
+  // Check the clickable prop
+  if (clickable.length < 1) {
+    throw Error('`clickable` property must contain at least one element id. Try passing an array of element ids to useClickAway({ clickable: [\'element1\', \'element2\'] })')
+  }
+
+  // Check for # symbol
+  if (reactAppId.includes('#')) console.error('`reactAppId` should not be prefixed with a `#` symbol.')
+  if (clickable.some(e => e.includes('#'))) console.error('Element ids in `clickable` array should not be prefixed with a `#` symbol.')
 
   function handleClick (e) {
     if (e && (e.path || e.composedPath())) {
